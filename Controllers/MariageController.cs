915dba5f76e4464d865419f6b86f6856ba886b68
Blackstar1509 <rgomseus@gmail.com>
2025-05-19 01:38:23 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/MariageController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QRCoder;
+using TIS.Core.Entities.Mariage;
+
+
+
+namespace TIS.Controllers
+{
+    public class MariageController : Controller
+    {
+        public List<TableMariage>? tables;
+
+            public  MariageController()
+        {
+            LoadTablesFromJson();
+        }
+
+        public void LoadTablesFromJson()
+        {
+            var jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/data/tables.json");
+            var json = System.IO.File.ReadAllText(jsonFilePath);
+            tables = JsonConvert.DeserializeObject<List<TableMariage>>(json);
+        }
+
+        public IActionResult Index(int? id)
+        {
+            ViewBag.TableId = id; 
+            return View(tables);
+        }
+
+        public IActionResult GenerateQr(int? id)
+        {
+            var table = tables.FirstOrDefault(t => t.Id == id);
+            if (table == null) return NotFound();
+
+            using (var qrGenerator = new QRCodeGenerator())
+            {
+                var url = Url.Action("Index", "Mariage", new { id = table.Id }, Request.Scheme);
+                using (var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q))
+                {
+                    using (var qrCode = new PngByteQRCode(qrCodeData))
+                    {
+                        var qrImage = qrCode.GetGraphic(20);
+                        return File(qrImage, "image/png");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Models/Mariage/TableMariage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIS.Core.Entities.Mariage
+{
+    public class TableMariage
+    {
+        #region Proprietes
+        public int? Id { get; set; }
+        public string? Nom { get; set; }
+        public string? Couleur { get; set; }
+        public string? PositionTop { get; set; }
+        public string? PositionLeft { get; set; }
+
+        #endregion
+    }
+}
